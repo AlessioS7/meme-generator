f53e24e34c4ae594ddfe93148811cfa1685f3660
AlessioS7 <alessio.santangelo7@hotmail.com>
2021-07-06 12:26:23 +0200

    Implemented USER APIs

---
+++ server/user-dao.js
@@ -0,0 +1,29 @@
+'use strict';
+/* Data Access Object (DAO) module for accessing users */
+
+const db = require('./db');
+const bcrypt = require('bcrypt');
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+exports.getUser = (username, password) => {
+  return new Promise((resolve, reject) => {
+    const sql = 'SELECT * FROM users WHERE username = ?';
+      db.get(sql, [username], (err, row) => {
+        if (err) {
+          reject(err);
+        } else if (row === undefined) {
+          resolve(false);
+        }
+        else {
+          // check the hashes with an async call, given that the operation may be CPU-intensive (and we don't want to block the server)
+          bcrypt.compare(password, row.hash).then(result => {
+            if(result) 
+              resolve(row.username);
+            else
+              resolve(false);
+          });
+        }
+    });
+  });
+};
